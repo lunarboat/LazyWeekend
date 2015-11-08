f2f8b49ba08aa5f0f457f8fb7785d4d43e1f05ae
lunarboat <lunarboat@outlook.com>
2015-11-08 15:52:14 +0800

    Initial Commit

---
+++ LazyWeekend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LazyWeekend
+//
+//  Created by lunarboat on 15/11/8.
+//  Copyright © 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LazyWeekend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LazyWeekend
+//
+//  Created by lunarboat on 15/11/8.
+//  Copyright © 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
